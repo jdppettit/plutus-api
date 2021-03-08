cbd0520205e60001088c4c36b7fa47604b4a114c
Joseph Pettit <jpettit@pettitservers.com>
2021-03-07 18:25:40 -0600

    add update account route

--- lib/plutus_web/router.ex
+++ lib/plutus_web/router.ex
@@ -11,6 +11,7 @@ defmodule PlutusWeb.Router do
    
     get "/account/linktoken", AccountController, :link_token
     post "/account", AccountController, :create
+    put "/account", AccountController, :update
     post "/account/exchange", AccountController, :exchange
     get "/account/:id", AccountController, :get
     get "/account", AccountController, :get_all
